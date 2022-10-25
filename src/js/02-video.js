1690bb02f07b51373c39e3a5e50f5f099de0d44a
Halai Daria <halaidaria@gmail.com>
2022-10-25 23:11:26 +0300

    second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,29 +1,23 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
 
-player.on('timeupdate', throttle(onTime,1000));
-function onTime(e) {
-    localStorage.setItem('videoplayer-current-time',JSON.stringify(e.seconds));
-}
-function onPlay(){
-    const savedTime =  localStorage.getItem('videoplayer-current-time');
-    if(savedTime){
-        player.setCurrentTime(savedTime).then(function(seconds) {
-            // seconds = the actual time that the player seeked to
+    const iframe = document.querySelector('iframe');
+    const player = new Player(this.iframe);
+
+     function updateTime()  {
+       const currentTime = player.setCurrentTime().then(function(seconds) {
+        localStorage.setItem("videoplayer-current-time", seconds);
         }).catch(function(error) {
             switch (error.name) {
                 case 'RangeError':
-                    // the time was less than 0 or greater than the video’s duration
+                    console.log(`Error`)
                     break;
-        
                 default:
-                    // some other error occurred
+                    player.on('timeupdate', throttle(this.updateTime, 1000));
                     break;
-            }
-        });
-    }
-}
-player.on('play',onPlay)
+            }});
+    };
+
+
+player.on('play',updateTime);
