2be62bd188ff0cdfd238aac669dc5fb79f9ec601
dariahalai <106152100+dariahalai@users.noreply.github.com>
2022-10-25 19:24:17 +0000

    Deploying to gh-pages from @ dariahalai/goit-js-hw-08@1666efadc6a2928975b33d6bd83c4567c6b42352 🚀

--- 03-feedback.69693347.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form");e.addEventListener("input",(function(e){const t=e.currentTarget.elements;t.email.value,t.message.value})),e.addEventListener("submit",(function(e){e.preventDefault(),localStorage.setItem("feedback-form-state",JSON.stringify(inputData)),console.log(inputData)}));
-//# sourceMappingURL=03-feedback.69693347.js.map
--- 03-feedback.b3ffad51.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form");e.addEventListener("input",(function(e){var t=e.currentTarget.elements;t.email.value,t.message.value})),e.addEventListener("submit",(function(e){e.preventDefault(),localStorage.setItem("feedback-form-state",JSON.stringify(inputData)),console.log(inputData)}))}();
-//# sourceMappingURL=03-feedback.b3ffad51.js.map
