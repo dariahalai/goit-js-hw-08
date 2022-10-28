2d61a98d9897772a2a1242c2494941899b08dbf3
Halai Daria <halaidaria@gmail.com>
2022-10-28 23:00:10 +0300

    task 2 changes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,23 +1,12 @@
-import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
-    const iframe = document.querySelector('iframe');
-    const player = new Player(this.iframe);
+const onPlay = function (data) {
+  const currentTime = localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+player.on('timeupdate', throttle(onPlay, 1000));
 
-     function updateTime()  {
-       const currentTime = player.setCurrentTime().then(function(seconds) {
-        localStorage.setItem("videoplayer-current-time", seconds);
-        }).catch(function(error) {
-            switch (error.name) {
-                case 'RangeError':
-                    console.log(`Error`)
-                    break;
-                default:
-                    player.on('timeupdate', throttle(this.updateTime, 1000));
-                    break;
-            }});
-    };
-
-
-player.on('play',updateTime);
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
